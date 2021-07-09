80221588072a0328d3f371db49baf8dccb2b54c9
najamusmulla <86570305+najamusmulla@users.noreply.github.com>
2021-07-09 20:26:37 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,14 @@
+package JavaWeek2Lab;
+
+public class Addition {
+	public static void main(String[] args) {
+		System.out.println("Addition of two Numbers:");
+		int first_number = 100;
+		int second_number = 200;
+		System.out.println("The First Number: " + first_number);
+		System.out.println("The Second Number: " + second_number);
+		// add two numbers
+		int sum = first_number + second_number;
+		System.out.println("The sum of First Number and Second Number is - " + sum);
+	}
+}
