2db413fa3bd228b7f14870c6d3fe7a1446789e1d
najamusmulla <86570305+najamusmulla@users.noreply.github.com>
2021-07-09 21:00:16 +0530

    Add files via upload

---
+++ PrintANumber.java
@@ -0,0 +1,16 @@
+package JavaWeek2Lab;
+
+import java.util.Scanner;
+
+public class PrintANumber {
+	public static void main(String[] args) {
+		// Creates a reader instance which takes
+		// input from standard input - keyboard
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter a Number: ");
+		// nextInt() reads the next integer from the keyboard
+		int number = reader.nextInt();
+		// println() prints the following line to the output screen
+		System.out.println("You entered: " + number);
+	}
+}
