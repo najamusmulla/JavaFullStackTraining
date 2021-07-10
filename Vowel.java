aaf520ef12c870013cfdc790f33bc0868cad94da
najamusmulla <86570305+najamusmulla@users.noreply.github.com>
2021-07-10 23:13:38 +0530

    Add files via upload

---
+++ Vowel.java
@@ -0,0 +1,14 @@
+package JavaWeek2Lab;
+
+public class Vowel {
+	public static void main(String[] args) {
+		char ch = 'k';
+		
+		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+			System.out.println(ch + " is a Vowel");
+			
+		} else {
+			System.out.println(ch + " is a Consonant");
+		}
+	}
+}
