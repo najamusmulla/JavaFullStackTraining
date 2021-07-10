63fbac0e0a66b89e02898af8337fff5d506170a7
najamusmulla <86570305+najamusmulla@users.noreply.github.com>
2021-07-10 22:50:21 +0530

    Add files via upload

---
+++ OddOrEven.java
@@ -0,0 +1,18 @@
+package JavaWeek2Lab;
+
+import java.util.Scanner;
+
+public class OddOrEven {
+	public static void main(String[] args) {
+		int num;
+		System.out.println("Please Enter a Number : ");
+		Scanner input = new Scanner(System.in);
+		num = input.nextInt();
+		if (num%2 == 0) {
+			System.out.println("The number entered is an Even Number");
+		}
+		else {
+			System.out.println("The number entered is an Odd Number");	
+		}
+	}
+}
