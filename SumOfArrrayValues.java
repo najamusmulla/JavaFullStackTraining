f910e8e5bc6476020cdf1bd69b0ba2bf70e7fe50
najamusmulla <86570305+najamusmulla@users.noreply.github.com>
2021-07-11 21:27:15 +0530

    Add files via upload

---
+++ SumOfArrrayValues.java
@@ -0,0 +1,13 @@
+package JavaWeek2Lab;
+
+public class SumOfArrrayValues {
+	public static void main(String args[]){
+		int[] array = {10, 20, 30, 40, 50, 10};
+		int sum = 0;
+		//Advanced for loop
+		for( int num : array) {
+			sum = sum+num;
+		}
+		System.out.println("Sum of array elements is:"+sum);
+	}
+}
