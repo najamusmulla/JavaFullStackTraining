682d59c70daeffaa251da0ebb193340f4bc30f39
najamusmulla <86570305+najamusmulla@users.noreply.github.com>
2021-07-25 16:54:40 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package JavaWeek3Lab;
+
+/*
+	* Question: 1. Write a program to print "Hello World" on Console.
+	* Author  : KNG21076 - Najamus Sahar M. Mulla
+*/
+
+public class HelloWorld {
+	public static void main (String args[]) {
+		System.out.println("Hello World...Welcome...Java");
+	}
+}
